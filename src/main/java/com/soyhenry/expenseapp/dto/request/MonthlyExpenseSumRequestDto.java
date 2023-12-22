4fe28999ccfa47e7edf926670788bc54ee065de4
Mili <101240663+MilagrosGuillot@users.noreply.github.com>
2023-12-22 17:45:35 -0300

    a

---
+++ src/main/java/com/soyhenry/expenseapp/dto/request/MonthlyExpenseSumRequestDto.java
@@ -0,0 +1,41 @@
+package com.soyhenry.expenseapp.dto.request;
+
+
+public class MonthlyExpenseSumRequestDto {
+    private int year;
+    private int month;
+    private double totalAmount;
+
+    // Constructores, getters y setters
+
+    public void MonthlyExpenseSumResponseDto(int year, int month, double totalAmount) {
+        this.year = year;
+        this.month = month;
+        this.totalAmount = totalAmount;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+}
+
---
+++ src/test/java/com/soyhenry/expenseapp/ExpenseAppApplicationTests.java
@@ -0,0 +1,91 @@
+package com.soyhenry.expenseapp;
+
+import com.soyhenry.expenseapp.controller.ExpenseController;
+import com.soyhenry.expenseapp.dto.response.ExpenseResponseDto;
+import com.soyhenry.expenseapp.dto.response.MonthlyExpenseSumResponseDto;
+import com.soyhenry.expenseapp.repository.ExpenseRepository;
+import com.soyhenry.expenseapp.service.ExpenseService;
+import com.soyhenry.expenseapp.service.impl.ExpenseServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class ExpenseAppApplicationTests {
+
+	@Mock
+	private ExpenseService expenseService;
+
+	@InjectMocks
+	private ExpenseController expenseController;
+
+	@Test
+	void testGetExpenses() {
+		// Mock de datos de respuesta
+		List<ExpenseResponseDto> mockResponses = Arrays.asList(
+				new ExpenseResponseDto(1, "Comida", 50.0),
+				new ExpenseResponseDto(2, "Transporte", 30.0)
+				// Puedes agregar más objetos según sea necesario
+		);
+
+		// Configurar el comportamiento esperado del servicio
+		when(expenseService.getAllExpenses()).thenReturn(mockResponses);
+
+		// Llamar al método bajo prueba
+		ResponseEntity<List<ExpenseResponseDto>> responseEntity = expenseController.getExpenses();
+
+		// Verificar el resultado
+		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+		assertEquals(mockResponses, responseEntity.getBody());
+	}
+	@Test
+	void testGetTotalExpenseSum() {
+		// Mock del resultado esperado
+		Double mockTotalSum = 150.0;
+
+		// Configurar el comportamiento esperado del servicio
+		when(expenseService.getTotalExpenseSum()).thenReturn(mockTotalSum);
+
+		// Llamar al método bajo prueba
+		ResponseEntity<Double> responseEntity = expenseController.getTotalExpenseSum();
+
+		// Verificar el resultado
+		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+		assertEquals(mockTotalSum, responseEntity.getBody());
+	}
+	@Test
+	void testGetExpenseById() {
+		// ID de gasto ficticio para la prueba
+		Long expenseId = 2L;
+
+		// Mock del resultado esperado
+		ExpenseResponseDto mockExpenseResponse = new ExpenseResponseDto(Math.toIntExact(expenseId), "Comida", 50.0);
+
+		// Configurar el comportamiento esperado del servicio
+		when(expenseService.getExpenseById(expenseId)).thenReturn(mockExpenseResponse);
+
+		// Llamar al método bajo prueba
+		ResponseEntity<ExpenseResponseDto> responseEntity = expenseController.getExpenseById(expenseId);
+
+		// Verificar el resultado
+		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+		assertEquals(mockExpenseResponse, responseEntity.getBody());
+	}
+}
